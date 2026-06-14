db1ff9d3f87228c0683bc2a084e083892d0caef9
MoatazMahmoud8 <moataz.alsbak86@gmail.com>
2026-06-14 16:34:15 +1000

    Fix Hermes compilation: enforce ES5 class transpilation with loose mode

--- babel.config.js
+++ babel.config.js
@@ -3,9 +3,11 @@ module.exports = function (api) {
   return {
     presets: ['babel-preset-expo'],
     plugins: [
-      '@babel/plugin-transform-class-properties',
-      '@babel/plugin-transform-private-methods',
-      '@babel/plugin-transform-private-property-in-object',
+      // Force transform all classes down to ES5 prototypes for Hermes compatibility
+      ['@babel/plugin-transform-classes', { "loose": true }],
+      ['@babel/plugin-transform-class-properties', { "loose": true }],
+      ['@babel/plugin-transform-private-methods', { "loose": true }],
+      ['@babel/plugin-transform-private-property-in-object', { "loose": true }],
       'react-native-reanimated/plugin',
     ],
   };
